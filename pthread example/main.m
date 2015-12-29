fa33e49ad4ad5559a686180cfaa87dcadac903e8
Ryan Farley <rfarley@wyzant.com>
2015-12-29 13:40:16 -0600

    Initial Commit

---
+++ pthread example/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pthread example
+//
+//  Created by Farley, Ryan on 12/29/15.
+//  Copyright © 2015 Farley, Ryan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
